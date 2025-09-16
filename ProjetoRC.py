2f7e88ad53466295666f7b5c0749cc74c7060c92
unknown <ebrilhantesz>
2025-09-16 18:57:03 -0300

    Versão 2.0, inicia-se a utilização de funções dentro do algoritmo, visando a modularização do sistema, maior organização do script e reaproveitamento de código.

--- ProjetoRC.py
+++ ProjetoRC.py
@@ -12,8 +12,8 @@ professores = []
 turmas = []
 matriculas = []
 
-while True:
-    # Menu de seleção principal (inicial)
+# Funções iniciais do sistema
+def mostrar_menu_principal():
     print("-- MENU PRINCIPAL --")
     print(" ")
     print("1. Gerenciamento de Alunos")
@@ -23,111 +23,125 @@ while True:
     print("5. Gerenciamento de Matriculas")
     print("9. Sair")
     print(" ")
-    opc = input("Informe a Opção Desejada: ")
+    return input("Informe a Opção Desejada: ")
+
+def mostrar_menu_alunos():
+    print("-- [ALUNOS] MENU DE OPERAÇÕES --")
     print(" ")
+    print("(1) Incluir")
+    print("(2) Listar")
+    print("(3) Atualizar")
+    print("(4) Excluir")
+    print("(9) Voltar ao Menu Principal")
+    print(" ")
+    return input("Informe a Opção Desejada: ")
+
+def cadastrar_alunos(alunos):
+    while True:
+        try:
+            codigo_aluno = int(input("Informe o Código do Aluno (apenas números): "))
+            break
+        except ValueError:
+            print("Entrada inválida! Por favor, digite apenas números.")
+    nome_aluno = input("Informe o Nome do Aluno: ")
+    cpf_aluno = input("Informe o CPF do Aluno: ")
+    dados_aluno = {}
+    dados_aluno["Código do Aluno"] = codigo_aluno
+    dados_aluno["Nome do Aluno"] = nome_aluno
+    dados_aluno["CPF do Aluno"] = cpf_aluno
+    alunos.append(dados_aluno)
+    print(" ")
+    print("Aluno incluído com sucesso!")
+    print(" ")
+
+def listar_alunos(alunos):
+    if len(alunos) == 0:
+        print("Não há alunos cadastrados.")
+        print(" ")
+    for nome_aluno in alunos:
+        print(nome_aluno)
+
+def atualizar_alunos(alunos):
+    while True:
+        try:
+            codigo_edicao_aluno = int(input("Qual o Código do Aluno que deseja editar? (apenas números): "))
+            break
+        except ValueError:
+            print("Entrada inválida! Por favor, digite apenas números.")
+    aluno_para_edicao = None
+    for dados_aluno in alunos:
+        if dados_aluno["Código do Aluno"] == codigo_edicao_aluno:
+            aluno_para_edicao = dados_aluno
+            break
+    if aluno_para_edicao is None:
+        print(f"O Aluno com código {codigo_edicao_aluno} não foi localizado.")
+        print("Os alunos cadastrados são:")
+    else:
+        # Validação do novo código do aluno
+        while True:
+            try:
+                aluno_para_edicao["Código do Aluno"] = int(input("Informe o novo Código do Aluno: "))
+                break
+            except ValueError:
+                print("Entrada inválida! Por favor, digite apenas números.")
+        aluno_para_edicao["Nome do Aluno"] = input("Informe o novo Nome do Aluno: ")
+        aluno_para_edicao["CPF do Aluno"] = input("Informe o novo CPF do Aluno: ")
+        print("Alteração realizada com sucesso!")
+        print(" ")
+        print("Os alunos cadastrados são:")
+    for dados_aluno in alunos:
+        print(dados_aluno)
+
+def excluir_alunos(alunos):
+    while True:
+        try:
+            codigo_exclusao_aluno = int(input("Qual o código do Aluno que deseja excluir?: "))
+            break
+        except ValueError:
+            print("Entrada inválida! Por favor, digite apenas números.")
+    aluno_para_exclusao = None
+    for dados_aluno in alunos:
+        if dados_aluno["Código do Aluno"] == codigo_exclusao_aluno:
+            aluno_para_exclusao = dados_aluno
+            break
+    if aluno_para_exclusao is None:
+        print(f"O Aluno com o código {codigo_exclusao_aluno} não foi localizado!")
+    else:
+        alunos.remove(aluno_para_exclusao)
+        print("Aluno excluído com sucesso!")
+        print("Os alunos cadastrados são:")
+    for dados_aluno in alunos:
+        print(dados_aluno)
+    if len(alunos) == 0:
+        print("Não há alunos cadastrados.")
+        print(" ")
+
+while True:
+    # Menu de seleção principal (inicial)
+    opc = mostrar_menu_principal()
 
     # Navegando pelo menu secundário (operações)
     if opc == '1':
         print("Você escolheu a opção: 1. Gerenciamento de Alunos")
         print(" ")
         while True:
-            print("-- [ALUNOS] MENU DE OPERAÇÕES --")
-            print(" ")
-            print("(1) Incluir")
-            print("(2) Listar")
-            print("(3) Atualizar")
-            print("(4) Excluir")
-            print("(9) Voltar ao Menu Principal")
-            print(" ")
-            alu = input("Informe a Opção Desejada: ")
-            print(" ")
+            alu = mostrar_menu_alunos()
             if alu == '1':
                 print("-- [ALUNOS] INCLUSÃO --")
                 print(" ")
-                # Validando o código do aluno
-                while True:
-                    try:
-                        codigo_aluno = int(input("Informe o Código do Aluno (apenas números): "))
-                        break
-                    except ValueError:
-                        print("Entrada inválida! Por favor, digite apenas números.")
-                nome_aluno = input("Informe o Nome do Aluno: ")
-                cpf_aluno = input("Informe o CPF do Aluno: ")
-                dados_aluno = {}
-                dados_aluno["Código do Aluno"] = codigo_aluno
-                dados_aluno["Nome do Aluno"] = nome_aluno
-                dados_aluno["CPF do Aluno"] = cpf_aluno
-                alunos.append(dados_aluno)
-                print(" ")
-                print("Aluno incluído com sucesso!")
-                print(" ")
+                cadastrar_alunos(alunos)
             elif alu == '2':
                 print("-- [ALUNOS] LISTAGEM --")
                 print(" ")
-                if len(alunos) == 0:
-                    print("Não há alunos cadastrados.")
-                    print(" ")
-                for nome_aluno in alunos:
-                    print(nome_aluno)
+                listar_alunos(alunos)
             elif alu == '3':
                 print("-- [ALUNOS] EDIÇÃO --")
                 print(" ")
-                # Validando o código do aluno para edição
-                while True:
-                    try:
-                        codigo_edicao_aluno = int(input("Qual o Código do Aluno que deseja editar? (apenas números): "))
-                        break
-                    except ValueError:
-                        print("Entrada inválida! Por favor, digite apenas números.")
-                aluno_para_edicao = None
-                for dados_aluno in alunos:
-                    if dados_aluno["Código do Aluno"] == codigo_edicao_aluno:
-                        aluno_para_edicao = dados_aluno
-                        break
-                if aluno_para_edicao is None:
-                    print(f"O Aluno com código {codigo_edicao_aluno} não foi localizado.")
-                    print("Os alunos cadastrados são:")
-                else:
-                    # Validação do novo código do aluno
-                    while True:
-                        try:
-                            aluno_para_edicao["Código do Aluno"] = int(input("Informe o novo Código do Aluno: "))
-                            break
-                        except ValueError:
-                            print("Entrada inválida! Por favor, digite apenas números.")
-                    aluno_para_edicao["Nome do Aluno"] = input("Informe o novo Nome do Aluno: ")
-                    aluno_para_edicao["CPF do Aluno"] = input("Informe o novo CPF do Aluno: ")
-                    print("Alteração realizada com sucesso!")
-                    print(" ")
-                    print("Os alunos cadastrados são:")
-                for dados_aluno in alunos:
-                    print(dados_aluno)
+                atualizar_alunos(alunos)
             elif alu == '4':
                 print("-- [ALUNOS] EXCLUSÃO --")
                 print(" ")
-                # Validando o código do aluno para exclusão
-                while True:
-                    try:
-                        codigo_exclusao_aluno = int(input("Qual o código do Aluno que deseja excluir?: "))
-                        break
-                    except ValueError:
-                        print("Entrada inválida! Por favor, digite apenas números.")
-                aluno_para_exclusao = None
-                for dados_aluno in alunos:
-                    if dados_aluno["Código do Aluno"] == codigo_exclusao_aluno:
-                        aluno_para_exclusao = dados_aluno
-                        break
-                if aluno_para_exclusao is None:
-                    print(f"O Aluno com o código {codigo_exclusao_aluno} não foi localizado!")
-                else:
-                    alunos.remove(aluno_para_exclusao)
-                    print("Aluno excluído com sucesso!")
-                    print("Os alunos cadastrados são:")
-                for dados_aluno in alunos:
-                    print(dados_aluno)
-                if len(alunos) == 0:
-                    print("Não há alunos cadastrados.")
-                    print(" ")
+                excluir_alunos(alunos)
             elif alu == '9':
                 print("Você escolheu voltar ao Menu Principal.")
                 break
